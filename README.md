4a168d2ce3d3970ae1ca344f31c1a622874037dc
Johnny <johnrussoofficial@outlook.com>
2019-05-17 10:09:31 -0400

    game complete, minor bugs still present

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ Timed jQuery-powered trivia game
 
 Logic finished. 
 
-To-do: add GIFs for answer page, write out questions to be asked
+Game Complete: only one minor bug left to address
 
 Theme: Parrot-themed trivia!
 
 Test yourself on your knowledge of our world's colorful, talkative,
-loud, yet loveable species of birds!
+loud, yet loveable species of birds! There are ten questions, each with clickable multiple-choice answers and a 15 second time limit. Try to get 10/10!
+
+KNOWN ISSUE: You cannot click on an answer until the timer appears at the top of the text field, just below the banner, which takes about a second. Please wait until you see the countdown before you choose an answer. I will look into trying to fix this minor bug
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,55 @@
+// Questions-answers-correct choices embedded in triviaQuestions data object
+
 var triviaQuestions = [{
-   question: "This is question one",
-   answers: ["first answer one", "first answer two", "first answer three", "first answer four"],
+   question: "1. The PartyParrot is real! What species is our favorite bird? (Hint: It's the largest species of parrot, by weight)",
+   answers: ["Kea", "Kakapo (Owl Parrot)", "Austral (Emerald) Parakeet", "Thick-billed Parrot"],
    rightAnswer: 1
 },{
-   question: "This is question two",
-   answers: ["second answer one", "second answer two", "second answer three", "second answer four"],
+   question: "2. To which one of these taxonomic superfamilies does the Cockatoo belong?",
+   answers: ["Psittacoidea", "Galliformes", "Cacatuoidea", "Strigopoidea"],
    rightAnswer: 2
 },{
-   question: "This is question three",
-   answers: ["third answer one", "third answer two", "third answer three", "third answer four"],
+   question: "3. What is the native continent of the budgie, AKA the parakeet?",
+   answers: ["North America", "South America", "Africa", "Australia"],
    rightAnswer: 3
 },{
-   question: "This is question four",
-   answers: ["fourth answer one", "fourth answer two", "fourth answer three", "fourth answer four"],
+   question: "4. What is the only species of parrot that lives in an Alpine mountain climate?",
+   answers: ["Kea", "Grey Parrot", "Hyacinth Macaw", "Ultramarine Lorikeet"],
    rightAnswer: 0
 },{
-   question: "This is question five",
-   answers: ["fifth answer one", "fifth answer two", "fifth answer three", "fifth answer four"],
+   question: "5. Macaws can get pretty big, but which species of Macaw is the biggest?",
+   answers: ["Red-and-Green Macaw", "Great Green Macaw", "Hyacinth Macaw", "Glaucous Macaw"],
    rightAnswer: 2
 },{
-   question: "This is question six",
-   answers: ["sixth answer one", "sixth answer two", "sixth answer three", "sixth answer four"],
+   question: "6. There is a unique black-feather parrot species from New Guinea that has natural drumming skills! What is it called?",
+   answers: ["Australian King Parrot", "Sulfur-crested Cockatoo", "St. Thomas Conure", "Palm Cockatoo"],
    rightAnswer: 3
 },{
-   question: "This is question seven",
-   answers: ["seventh answer one", "seventh answer two", "seventh answer three", "seventh answer four"],
+   question: "7. What escaped former-pet parrot can you find living within feral communities in Brooklyn, New York?",
+   answers: ["Monk Parakeet", "Cockatiel", "Rosella", "Horned Parakeet"],
    rightAnswer: 0
 },{
-   question: "This is question eight",
-   answers: ["eighth answer one", "eighth answer two", "eighth answer three", "eighth answer four"],
+   question: "8. According to Guinness World Records, the world's oldest parrot, a cockatoo named Cookie, lived to be about how old?",
+   answers: ["79 years old", "a little over 99 years old", "82 to 83 years old", "almost 103 years old!"],
    rightAnswer: 2
 },{
-   question: "This is question nine",
-   answers: ["ninth answer one", "ninth answer two", "ninth answer three", "ninth answer four"],
+   question: "9. All parties must come to an end... Which of these parrots is now extinct?",
+   answers: ["Glaucous Macaw", "Major Mitchell's Cockatoo", "Edward's Fig Parrot", "Shell Parakeet"],
    rightAnswer: 0
 },{
-   question: "This is question ten",
-   answers: ["tenth answer one", "tenth answer two", "tenth answer three", "tenth answer four"],
+   question: "10. Which one of these statements below is true about most PartyParrots? (you may be surprised!)",
+   answers: ["They are omnivores", "They are monogamous and form life-long mating pairs", "They don't build nests", "All of the above"],
    rightAnswer: 3
 }];
 
-var picResponse = ["qOne", "qTwo", "qThree", "qFour", "qFive", "qSix", "qSeven", "qEight", "qNine", "qTen"];
+// Second array handles .GIFs seen on the answer page after each question
+var picResponse = ["question-one", "question-two", "qThree", "qFour", "qFive", "qSix", "qSeven", "qEight", "qNine", "qTen"];
 var rightPick = "";
 var wrongPick = "";
 var noAnswer = "";
 var round = "";
 
-
+// handles first button on start page
 $(document).on("click", "#start-game", function() {
    $("#begin-page").hide();
    newGame();
@@ -60,12 +63,15 @@ function newGame() {
    nextQuestion();
 }
 
+// Creates each new question page an registers right and 
+// wrong guesses plus out-of-time conditions.
+// Most of game's logic is here
 function nextQuestion() {
    $("#answer-page").hide();
    $("#question-page").show();
    $("#time-left").text("");
    var yourGuess = "";
-   var seconds = 20;
+   var seconds = 16;
    var time = setInterval(function() {
       seconds -=1;
       console.log(seconds);
@@ -105,6 +111,8 @@ function nextQuestion() {
 
 }
 
+// Make's each page after a questions is answered or unanswered. Each page is
+// different depending on player being right, wrong or out of time
 function getAnswer (yourGuess) {
    $("#question-page").hide();
    $("#answer-page").show();
@@ -125,17 +133,21 @@ function getAnswer (yourGuess) {
       $("#right-wrong").html("<h2>" + "Uh-oh! Out of time" + "</h2>");
       $("#correct-choice").html("<p>" + "the right choice was: " + triviaQuestions[round].answers[triviaQuestions[round].rightAnswer] + "</p>");
    }
+   
+   $("#reward-pic").html("<img src='assets/images/" + picResponse[round] + ".gif' />");
 
    if(round === 9) {
-      setTimeout(finalScore, 4000);
+      setTimeout(finalScore, 4500);
    } else {
       round +=1;
-      setTimeout(nextQuestion, 4000);
+      setTimeout(nextQuestion, 4500);
    }
 
 
 }
 
+// Creates the final scoreboard and makes another button 
+// that allows the player to restart.
 function finalScore() {
    var newBtn = $("<input/>").attr({ 
       type: "button",
